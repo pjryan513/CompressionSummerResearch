9034f5f791acfb2601448cdeb5c75384a3a4ab93
Patrick Ryan <pjryan@pugetsound.edu>
2017-06-30 13:46:56 -0700

    commit to pull

--- Our_Code/BBCCompressor.c
+++ Our_Code/BBCCompressor.c
@@ -20,7 +20,7 @@ void bbcCompress(blockSegBBC *param){
       param->run_type = getHeadType(param->header);
       //either ZERO_FILL or ONE_FILL 00000000 or 11111111
       param->fill_bit = getFillByte(param->header); //actually represents the fill bit, saved as a byte to compare more easily
-    }
+
   /**
   ***FUNCTION DOCUMENTATION***
 
@@ -63,119 +63,84 @@ void bbcCompress(blockSegBBC *param){
     if(param->header==NULL){
       startNewRun(param->byte_type);
 
-<<<<<<< HEAD
     }
-=======
-      //increments the fill length in the header
-      //increments the counter bytes in a type 3 run
-      incrementFill();
->>>>>>> 7740c9e6d983735e98a9988cb668174d6b139a25
-
     //0-fill byte or 1-fill byte (11111111 or 00000000)
-    else if(param->byte_type == ZERO_BYTE || param->byte_type == ONE_BYTE)
+    else if(param->byte_type == ZERO_BYTE || param->byte_type == ONE_BYTE){
 
-<<<<<<< HEAD
       //proper type of fill (0 or 1)
-      if(param->byte_type == param->fill_bit)
-=======
-      
->>>>>>> 7740c9e6d983735e98a9988cb668174d6b139a25
+      if(param->byte_type == param->fill_bit){
 
         //if we are currently in a TYPE_1 run
-        if(param->run_type == TYPE_1)
+        if(param->run_type == TYPE_1){
 
           //we're not too long yet, so stay as a TYPE_1 and incrememt
-          if(param->fill_len < FILL_LIMIT)
+          if(param->fill_len < FILL_LIMIT){
             incrementFill();
 
-<<<<<<< HEAD
-          //otherwise, change to type 3 run
-          else
+          }
+          else{ //otherwise, change to type 3 run
             changeRunType(TYPE_3);
-=======
-*/
-  //if the header is null
-  if(header==NULL){
-    startNewRun(byte_type);
->>>>>>> 7740c9e6d983735e98a9988cb668174d6b139a25
-
-        //if we are currently in a TYPE_3 run
-        else if(param->run_type = TYPE_3)
+          }
+        }
+        else if(param->run_type = TYPE_3) //if we are currently in a TYPE_3 run
           //increment the counter bytes
           incrementCounterByte();
 
-      //if it's not the right kind of fill, start a new run
-      else
+      }
+      else{ //if it's not the right kind of fill, start a new run
 
         startNewRun(param->byte_type);
-
+      }
     //odd byte (Eg: 00010000)
-    else if(param->byte_type==ODD_BYTE)
+    }
+    else if(param->byte_type==ODD_BYTE){
       /*if the tail_length is 0, we can easily change to
       either a TYPE_2 or TYPE_4 run by concatenating the
       ODD_BYTE to the end of our fill. */
-      if(param->tail_len == 0)
+      if(param->tail_len == 0){
 
         //if we are a TYPE_1 run
-        if(param->run_type == TYPE_1)
+        if(param->run_type == TYPE_1){
 
           //change ourselves to a TYPE_2 run
           //this will end the current run
           changeRunType(TYPE_2);
 
         //if we are a TYPE_3 run
-        if(param->run_type == TYPE_3)
+        }
+        else if(param->run_type == TYPE_3){
 
           //change ourselves to a TYPE_4 run
           //this will end the current run
           changeRunType(TYPE_4);
-
+        }
       /*if we already have a tail, we must start a new run using
       the ODD_BYTE*/
       //the startNewRun() function checks for this specific case
-      else
+      }
+      else{
         startNewRun(param->byte_type);
-
-    //messy byte (Eg: 11010100)
-    else if(param->byte_type==TYPE_4)
+      }
+    }
+    else if(param->byte_type==TYPE_4){ //messy byte (Eg: 11010100)
 
       //if we are a TYPE_1 run
-      if(param->run_type == TYPE_1)
+      if(param->run_type == TYPE_1 || param->run_type == TYPE_3){
 
-        if(tail_length<TAIL_LIMIT)
+        if(tail_length<TAIL_LIMIT){
 
           //if we aren't too long yet, add the
           //messy bit to the tail and increment the tail length
           incrementTail(param->next_byte);
+        }
 
-<<<<<<< HEAD
-        else
-=======
-    /*if we already have a tail, we must start a new run using
-    the ODD_BYTE*/
-    //the startNewRun() function checks for this specific case 
-    else
-      startNewRun(byte_type);
-
-  //messy byte (Eg: 11010100)
-  else if(byte_type==3)
->>>>>>> 7740c9e6d983735e98a9988cb668174d6b139a25
-
-          //otherwise, start a new run
-          startNewRun(param->byte_type);
-
-      //if we are a TYPE_3 run
-      else if(param->run_type == TYPE_3)
-
-        //if we are not too long yet, add the
-        //messy bit to the tail and increment the tail length
-        if(param->tail_length<TAIL_LIMIT)
-
-          incrementTail(param->next_byte);
-
-        //otherwise, start a new run
-        else
-
-          startNewRun(param->byte_type);
+        /*if we already have a tail, we must start a new run using
+        the ODD_BYTE*/
+        //the startNewRun() function checks for this specific case
+      }
+        else{
+          startNewRun(byte_type);
+        }
+    }
   }
 }
