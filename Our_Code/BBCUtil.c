d5c75566fb3092a96ae026581352a70733fb8d62
Patrick Ryan <pjryan@pugetsound.edu>
2017-07-03 13:06:04 -0700

    util changes

--- Our_Code/BBCUtil.c
+++ Our_Code/BBCUtil.c
@@ -16,35 +16,45 @@ void startNewRun(blockSegBBC *param){
   //ZERO FILL
   //there's only one possible byte we should produce TYPE_1
   if(param->byte_type == ZERO_BYTE)
+  {
     param->header = 0b10010000;
 	  param->fill_len = 1;
     param->tail_len = 0;
     param->run_type = TYPE_1;
     param->fill_bit = 0b00000000;
+  }
 
   //ONE_FILL
   //there's only one possible byte we should produce TYPE_1
   if(param->byte_type == ONE_BYTE)
+  {
     param->header = 0b11010000;
     param->fill_len = 1;
     param->tail_len = 0;
     param->run_type = TYPE_1;
     param->fill_bit = 0b11111111;
+  }
 
   //ODD BYTE
   if(param->byte_type == ODD_BYTE)
+  {
     //make (and end) type 2 run with the odd bit stored in the header
     param->header = 0b01000000;
     //here we decide the last three bits of the above binary number
     param->header = placeOddBit(param);
+    param->run_type = TYPE_2;
+  }
 
   //MESSY BYTE... for example, 01101010
   if(param->byte_type == MESSY_BYTE)
+  {
     //start off with a type 1 run, fill_length = 0.
     param->header = 0b1X000001;
     //store the header byte and the literal in the run
     param->curr_run = {'1X000001', '01101010'};
+    param->run_type = TYPE_1;
   }
+}
 
 //changes the run type (either type 1 to type 2 or type 3 to type 4)
 void changeRunType(int run_type, blockSegBBC *param){
@@ -54,12 +64,14 @@ void changeRunType(int run_type, blockSegBBC *param){
   if(run_type == TYPE_2){
     //we want to preserve: fill bit and the fill_length.
     //get the odd bit position from
-    param->header = '01' + fill_bit + fill_length + odd_bit_pos
+    param->header = '01' + fill_bit + fill_length + odd_bit_pos;
+    param->run_type = TYPE_2;
   }
   // if a type 1 ever max's out the fill_length than it must be changed to a type 3
   else if(run_type == TYPE_3)
   {
-    param->header = '001' + fill_bit + tail_length //counter bytes will follow
+    param->header = '001' + fill_bit + tail_length; //counter bytes will follow
+    param->run_type = TYPE_3;
   }
 
   /*this only happens if we are already a TYPE_3 run AND the
@@ -67,6 +79,7 @@ void changeRunType(int run_type, blockSegBBC *param){
   else if(run_type == TYPE_4){
 	//we want to preserve: fill bytes and the fill_length.
     param->header = '0001' + fill_bit + odd_bit_pos  //counter bytes will follow
+    param->run_type = TYPE_4
   }
 
 }
@@ -81,19 +94,18 @@ byte placeOddBit(blockSegBBC *param){
 //increments the fill length in the header
 //increments the counter bytes in a type 3 run
 void incrementFill();
-
-//changes the current run type to the desired header type
-void changeRunType(int type){
-
+{
+  
 }
 
 void getType(next_byte){
+
 
 }
 
 void getFill()
 {
-  
+
 }
 
 void getTail();
